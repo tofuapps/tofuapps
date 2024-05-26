95c545f0dc8398f26dd59c88efa6ece139ff0dd3
Wern <dev@wern.cc>
2024-05-26 13:28:07 +0800

    minor info tweaks

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -3,7 +3,7 @@
 	We're a small team that loves to create software for the community. With complementing experiences in software development and user experience design, we're excited to bring you some of our best work.
 </p>
 <div id="about-founders-grid">
-	<a href="https://l-yc.me/" target="_blank" class="about-founder">
+	<a href="https://yuechen.li/" target="_blank" class="about-founder">
 		<img src="./logos/yuechen.jpg" alt="Yue Chen"/>
 		<h3>Yue Chen</h3>
 		<span>Co-Founder</span>
@@ -12,7 +12,7 @@
 				<svg class="inline-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
 					<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z" clip-rule="evenodd" />
 				</svg>
-				l-yc.me
+				yuechen.li
 		</span>
 	</a>
 	<a href="https://wern.cc/" target="_blank" class="about-founder">
